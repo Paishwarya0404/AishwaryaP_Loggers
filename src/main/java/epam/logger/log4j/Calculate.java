573ddbc5bdae4ffb1f878c9e14f4cb96d0f4fa3b
Paishwarya0404 <aishwarya.p044@gmail.com>
2020-02-26 21:21:41 +0530

    first commit

---
+++ src/main/java/epam/logger/log4j/Calculate.java
@@ -0,0 +1,8 @@
+package epam.logger.log4j;
+
+public class Calculate {
+	public static void main(String[] args) {
+	Construction hs = new Construction();
+	hs.dimensions();
+	}
+}
---
+++ src/main/java/epam/logger/log4j/House.java
@@ -0,0 +1,11 @@
+package epam.logger.log4j;
+
+public class House extends Interest {
+
+	public static void main(String[] args) {
+		House invoke = new House();
+    	invoke.inputDetails();
+
+	}
+
+}
